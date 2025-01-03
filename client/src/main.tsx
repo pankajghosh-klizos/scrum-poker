ba5dba22411c463d451463545684cc7a7e7215f6
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-03 12:20:48 +0530

    set up Redux store and update project structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./store/store.ts";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
