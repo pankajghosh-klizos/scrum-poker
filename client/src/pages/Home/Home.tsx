581a0ed6fb90b20a830267e31ba1070e8c89aca8
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-06 16:09:49 +0530

    refactor Home and Root components; improve layout and update image dimensions

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -7,29 +7,28 @@ const Home = () => {
 
   return (
     <Container>
-      <div className="d-flex flex-column align-content-center justify-content-center pt-4 pt-md-5">
-        <h2 className="mb-md-4 text-center display-2 lh-1">
+      <div className="d-flex flex-column align-items-center justify-content-center pt-4 pt-md-5 gap-4">
+        <h2 className="m-0 text-center display-2 lh-1">
           <span className="d-block fw-semibold">Scrum Poker</span>for teams
         </h2>
 
-        <p className="m-0 text-black-50 fs-5 text-center mb-3 mb-md-4">
+        <p className="m-0 text-black-50 fs-5 text-center">
           Easy-to-use and fun story point estimations.
         </p>
 
         <Button
-          className="btn-primary mx-auto px-md-5 py-md-2 mb-4"
+          className="btn-primary px-5 py-md-2"
           onClick={() => navigate("/new-game")}
         >
           Start a New Game
         </Button>
 
-        <div className="border border-light-subtle px-5 py-4 rounded-4 shadow-sm">
+        <div className="border border-light-subtle px-5 py-4 rounded-4 shadow-sm mb-4">
           <img
             src={HomeBanner}
             alt="banner"
-            width="346px"
-            height="455px"
-            className="object-fit-cover h-100 w-100"
+            width="400px"
+            className="object-fit-cover w-100"
           />
         </div>
       </div>
--- client/src/pages/Root/Root.tsx
+++ client/src/pages/Root/Root.tsx
@@ -5,7 +5,7 @@ const Root = () => {
   return (
     <div className="min-vh-100 w-100 d-flex flex-column align-items-center justify-content-center">
       <Header />
-      <main className="flex-grow-1">
+      <main className="flex-grow-1 w-100">
         <Outlet />
       </main>
       <Footer />
