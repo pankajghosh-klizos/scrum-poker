98e5d376de823c9c0c69d26f86ce72aaf3740a5e
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-06 15:41:49 +0530

    conditionally render 'Start New Game' button in Header component based on current path

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,17 +1,23 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { Button, Container } from "../../components";
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <header className="border-bottom border-light-subtle w-100">
       <Container>
         <nav className="d-flex gap-5 align-items-center justify-content-between py-3 py-md-4">
           <h2 className="m-0 fs-2 fw-bold">Scrum Poker</h2>
-          <Button className="btn-primary" onClick={() => navigate("/new-game")}>
-            Start New Game
-          </Button>
+          {location.pathname === "/" && (
+            <Button
+              className="btn-primary"
+              onClick={() => navigate("/new-game")}
+            >
+              Start New Game
+            </Button>
+          )}
         </nav>
       </Container>
     </header>
