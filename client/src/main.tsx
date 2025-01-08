f54ea6e95c0ce6319c5cf296d993ccda8b442a56
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-08 16:07:15 +0530

    refactor: remove StrictMode wrapper from main rendering for cleaner structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Provider } from "react-redux";
@@ -10,28 +9,26 @@ import "./index.css";
 import { SocketProvider } from "./context/SocketContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route element={<Root />}>
-            <Route index element={<Home />} />
-            <Route path="new-game" element={<NewGame />} />
-            <Route path="join/:roomId" element={<JoinGame />} />
+  <BrowserRouter>
+    <Provider store={store}>
+      <Routes>
+        <Route element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="new-game" element={<NewGame />} />
+          <Route path="join/:roomId" element={<JoinGame />} />
 
-            <Route
-              path="play"
-              element={
-                <ProtectedRoute>
-                  <SocketProvider>
-                    <PlayGame />
-                  </SocketProvider>
-                </ProtectedRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
+          <Route
+            path="play"
+            element={
+              <ProtectedRoute>
+                <SocketProvider>
+                  <PlayGame />
+                </SocketProvider>
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </Provider>
+  </BrowserRouter>
 );
