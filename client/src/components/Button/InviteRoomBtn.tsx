146e64588c59aaa6db45afaad5290560d268a3ea
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-09 13:04:24 +0530

    fix: add clipboard.js for improved invite link copying functionality

--- client/src/components/Button/InviteRoomBtn.tsx
+++ client/src/components/Button/InviteRoomBtn.tsx
@@ -1,23 +1,32 @@
 import { useSelector } from "react-redux";
 import Button from "./Button";
 import toast from "react-hot-toast";
+import ClipboardJS from "clipboard";
 
 const InviteRoomBtn = () => {
   const { room } = useSelector((state: any) => state.room);
   const baseUrl = window.location.origin;
 
-  const handleInviteRoom = async () => {
-    await navigator.clipboard
-      .writeText(`${baseUrl}/join/${room.roomId}`)
-      .then(() => {
-        toast.success("Invite link copied.");
-      });
+  const handleInviteRoom = () => {
+    const clipboard = new ClipboardJS(".btn-invite", {
+      text: () => `${baseUrl}/join/${room.roomId}`,
+    });
+
+    clipboard.on("success", () => {
+      toast.success("Invite link copied.");
+      clipboard.destroy();
+    });
+
+    clipboard.on("error", () => {
+      toast.error("Failed to copy invite link.");
+      clipboard.destroy();
+    });
   };
 
   return (
     <Button
       onClick={handleInviteRoom}
-      className="btn-secondary px-3 d-flex gap-3 align-items-center justify-content-center"
+      className="btn-invite btn-secondary px-3 d-flex gap-3 align-items-center justify-content-center"
     >
       <span>Invite</span>
     </Button>
