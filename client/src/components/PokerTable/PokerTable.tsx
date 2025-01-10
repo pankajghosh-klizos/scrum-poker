d0a4adaf3030e521368ed02c9bf86c25d990ac10
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-10 13:46:52 +0530

    fix: update PokerTable component to conditionally render card selection prompt and reveal button

--- client/src/components/PokerTable/PokerTable.tsx
+++ client/src/components/PokerTable/PokerTable.tsx
@@ -1,9 +1,12 @@
 import { useSelector } from "react-redux";
 import Participant from "../Participant/Participant";
 import "./PokerTable.css";
+import RevealBtn from "../Button/RevealBtn";
 
 const PokerTable = () => {
   const { room } = useSelector((state: any) => state.room);
+  const { participant } = useSelector((state: any) => state.participant);
+
   return (
     <div className="d-grid gap-3 gap-md-4">
       <div className="participants d-flex gap-3 gap-md-5 align-items-center justify-content-center">
@@ -20,14 +23,17 @@ const PokerTable = () => {
           className="poker-table d-flex align-items-center justify-content-center bg-primary-subtle rounded-4 p-5"
           style={{}}
         >
-          <p className="m-0 text-muted fs-5 text-center">Pick your cards!</p>
+          {!participant.isCardSelected && (
+            <p className="m-0 text-muted fs-5 text-center">Pick your cards!</p>
+          )}
+          {participant.role == "admin" && <RevealBtn />}
 
-          {/* <RevealBtn /> */}
-
-          {/* <p className="m-0 fs-5 text-center">
-            <span className="d-block">Average Score</span>
-            <span className="fw-bold fs-3">0</span>
-          </p> */}
+          {room.average > 0 && (
+            <p className="m-0 fs-5 text-center">
+              <span className="d-block">Average Score</span>
+              <span className="fw-bold fs-3">{room.average}</span>
+            </p>
+          )}
         </div>
 
         <Participant participant={room?.participants[1]} />
