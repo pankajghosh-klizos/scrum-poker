753fded8e3d8ee10c3ee725eeaa36e52d95fdb95
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-10 12:29:43 +0530

    fix: enhance CardDeck component with conditional styling for selected cards

--- client/src/components/CardDeck/CardDeck.tsx
+++ client/src/components/CardDeck/CardDeck.tsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import Card from "../Card/Card";
-import { useState } from "react";
 
 const CardDeck = () => {
   const { room } = useSelector((state: any) => state.room);
@@ -8,7 +7,6 @@ const CardDeck = () => {
   const cardOptions = room?.votingSystem.split(", ");
   const firstOption = cardOptions[0]?.slice(-1);
   const lastOption = cardOptions[cardOptions.length - 1]?.slice(0, 1);
-  const [loading, setLoading] = useState(false);
 
   const handleVote = (value: string) => {
     console.log(value);
@@ -17,19 +15,39 @@ const CardDeck = () => {
   return (
     <ul
       className={`list-unstyled d-flex flex-wrap gap-2 justify-content-center ${
-        participant?.selectedCard ? "pe-none" : ""
+        participant?.selectedCard ? "pe-none opacity-50" : ""
       }`}
     >
       <li>
-        <Card value={firstOption} onClick={() => handleVote(firstOption)} />
+        <Card
+          value={firstOption}
+          onClick={() => handleVote(firstOption)}
+          className={`${
+            participant?.selectedCard == firstOption
+              ? "bg-secondary-subtle"
+              : ""
+          }`}
+        />
       </li>
       {cardOptions.slice(1, -1).map((option: string) => (
         <li key={option}>
-          <Card value={option} onClick={() => handleVote(option)} />
+          <Card
+            value={option}
+            onClick={() => handleVote(option)}
+            className={`${
+              participant?.selectedCard == option ? "bg-secondary-subtle" : ""
+            }`}
+          />
         </li>
       ))}
       <li>
-        <Card value={lastOption} onClick={() => handleVote(lastOption)} />
+        <Card
+          value={lastOption}
+          onClick={() => handleVote(lastOption)}
+          className={`${
+            participant?.selectedCard == lastOption ? "bg-secondary-subtle" : ""
+          }`}
+        />
       </li>
     </ul>
   );
