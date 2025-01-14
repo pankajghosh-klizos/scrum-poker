f655c226468bc3287a2b3562bcb5d2e32a25d846
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2025-01-14 13:18:29 +0530

    feat: add loading spinner to card selection in CardDeck component

--- client/src/components/CardDeck/CardDeck.tsx
+++ client/src/components/CardDeck/CardDeck.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
+import { FiRefreshCw } from "react-icons/fi";
 
 import Card from "../Card/Card";
 import { selectCard } from "../../api";
 import { setSelectedCard } from "../../store/slices/participant.slice";
+import "./CardDeck.css";
 
 const CardDeck = () => {
   const dispatch = useDispatch();
@@ -14,66 +16,88 @@ const CardDeck = () => {
   const cardOptions = room?.votingSystem.split(", ");
   const firstOption = cardOptions[0]?.slice(-1);
   const lastOption = cardOptions[cardOptions.length - 1]?.slice(0, 1);
-  const [loading, setLoading] = useState(false);
+  const [loadingCard, setLoadingCard] = useState<string | null>(null);
 
   const handleVote = async (data: { card: string }) => {
-    setLoading(true);
+    setLoadingCard(data.card); // Set the card that is being loaded
     try {
       dispatch(setSelectedCard(data.card));
       const res = await selectCard(data);
 
       if (!res?.success) {
-        toast.error("Error select card");
+        toast.error("Error selecting card");
         dispatch(setSelectedCard(""));
       }
     } catch (error) {
-      console.error("Error select card", error);
-      toast.error("Error select card");
+      console.error("Error selecting card", error);
+      toast.error("Error selecting card");
       dispatch(setSelectedCard(""));
     } finally {
-      setLoading(false);
+      setLoadingCard(null); // Reset loading state
     }
   };
 
   return (
     <ul
-      className={`list-unstyled d-flex flex-wrap gap-2 justify-content-center ${
+      className={`card-deck list-unstyled d-flex flex-wrap gap-2 justify-content-center ${
         participant?.isCardSelected || room?.average > 0
           ? "pe-none opacity-75"
           : ""
       }`}
     >
-      <li>
+      <li className="d-flex position-relative align-items-center justify-content-center">
         <Card
           value={firstOption}
           onClick={() => handleVote({ card: firstOption })}
           className={`${
-            participant?.selectedCard == firstOption
+            participant?.selectedCard === firstOption
               ? "bg-secondary-subtle"
               : ""
           }`}
         />
+        {loadingCard === firstOption && (
+          <span className="position-absolute opacity-50">
+            <FiRefreshCw className="spin" />
+          </span>
+        )}
       </li>
+
       {cardOptions.slice(1, -1).map((option: string) => (
-        <li key={option}>
+        <li
+          key={option}
+          className="d-flex position-relative align-items-center justify-content-center"
+        >
           <Card
             value={option}
             onClick={() => handleVote({ card: option })}
             className={`${
-              participant?.selectedCard == option ? "bg-secondary-subtle" : ""
+              participant?.selectedCard === option ? "bg-secondary-subtle" : ""
             }`}
-            disabled={loading}
+            disabled={loadingCard !== null}
           />
+          {loadingCard === option && (
+            <span className="position-absolute opacity-50">
+              <FiRefreshCw className="spin" />
+            </span>
+          )}
         </li>
       ))}
-      <li>
+
+      <li className="d-flex position-relative align-items-center justify-content-center">
         <Card
           value={lastOption}
           onClick={() => handleVote({ card: lastOption })}
           className={`${
-            participant?.selectedCard == lastOption ? "bg-secondary-subtle" : ""
+            participant?.selectedCard === lastOption
+              ? "bg-secondary-subtle"
+              : ""
           }`}
         />
+        {loadingCard === lastOption && (
+          <span className="position-absolute opacity-50">
+            <FiRefreshCw className="spin" />
+          </span>
+        )}
       </li>
     </ul>
   );
